09eb4a49675d0a728d516cffcf43085376e729c0
Azamat Usmanov <usmanov.ak@phystech.edu>
2022-09-13 11:02:08 +0300

    rm

--- interactive.h
+++
@@ -1,8 +0,0 @@
-#ifndef INTERACTIVE_SRC_H
-#define INTERACTIVE_SRC_H
-
-void introduction();
-void skip_line();
-void input_coefficients(double *a, double *b, double *c);
-
-#endif  // INTERACTIVE_SRC_H
--- test.h
+++
@@ -1,7 +0,0 @@
-#ifndef TEST_SRC_H_
-#define TEST_SRC_H_
-
-void test_quadratic_equation(double a, double b, double c, int test_number_of_roots, double test_x1, double test_x2);
-void unit_tests();
-
-#endif  // TEST_SRC_H_
